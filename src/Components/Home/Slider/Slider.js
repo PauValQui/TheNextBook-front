64b6097ecd74c8b6991e983675b7007a0553af5e
Paulavq <paulavq103@gmail.com>
2022-05-21 21:58:41 +0200

    Slider Terminado

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 import './Slider.scss'
 import Imagen1 from '../../../Img/Slider1.png';
 import Imagen2 from '../../../Img/Slider2.png';
@@ -9,21 +9,79 @@ import Imagen5 from '../../../Img/Slider5.png';
 import {ReactComponent as FlechaIzquierda} from '../../../Img/iconmonstr-arrow-left.svg'
 import {ReactComponent as FlechaDerecha} from '../../../Img/iconmonstr-arrow-right.svg'
 
-
 const Slider = () =>  {
-    const slide =useRef(null);
+    const slide = useRef(null);
+    const intervaloSlider = useRef(null);
 
     const siguiente = () => {
-        console.log(slide)
+        //Compruebo que el slide tenga elementos
+        if(slide.current.children.length > 0){
+            //Busco el primer elemento del slide
+            const primerElemento = slide.current.children[0];
+
+            //Le doy la transicion
+            slide.current.style.transition = `1ms ease-out all`;
+
+            const tamañoSlide = slide.current.children[0].offsetWidth;
+
+            //Muevo el slide
+            slide.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+            const transicion = () => {
+				// Reiniciamos la posicion del Slideshow.
+				slide.current.style.transition = 'none';
+				slide.current.style.transform = `translateX(0)`;
+
+				// Tomamos el primer elemento y lo mandamos al final.
+				slide.current.appendChild(primerElemento);
+
+				slide.current.removeEventListener('transitionend', transicion);
+			}
+
+			// addEventlistener para cuando termina la animacion.
+			slide.current.addEventListener('transitionend', transicion);
+        }
     }
 
     const anterior = () => {
-        console.log('Anterior')
+        if (slide.current.children.length>0){
+            //Obtengo el tamaño del slide 
+            const tamaño = slide.current.children.length-1;
+            //busco el ultimo elemento del slide
+            const ultimoElemento = slide.current.children[tamaño]
+            //Insertamos el ultimo elemento en el primero para volver al anterior
+            slide.current.insertBefore(ultimoElemento, slide.current.firstChild)
+            
+            slide.current.style.transition='none';
+            const tamañoSlide = slide.current.children[0].offsetWidth;
+            //Muevo el slide
+            slide.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+
+            setTimeout(() => {
+                slide.current.style.transition=`1ms ease-out all`;
+                slide.current.style.transform = `translateX(0)`;
+
+            },30);
+        }
     }
 
+    useEffect(() => {
+        intervaloSlider.current = setInterval(() => {
+            siguiente();
+        }, 5000);
+
+        //Elimino intervalos
+        slide.current.addEventListener('mousseenter', ()=>{
+            clearInterval(intervaloSlider.current);
+        })
+
+        
+    }, []);
+
   return (
-    <div className="Slider" ref={slide}>
-      <div className="Slider__ContenedorSlider">
+    <div className="Slider">
+      <div className="Slider__ContenedorSlider" ref={slide}>
           <div className="Slider__ContenedorSlider__Slider">
             <a href="" className="Slider__Enlace">
               <img src={Imagen1} alt="Img1" className="ImagenSlider"/>
